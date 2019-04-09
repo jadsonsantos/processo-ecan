9f39ea17993ec3fb7c3db41595769ee3cb609697
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-04-09 18:21:46 -0300

    Add watch para sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 
-gulp.task('sass', function () {
+function compilaSass() {
     return gulp.src('./scss/**/*.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
       .pipe(gulp.dest('./css'));
-});
+}
+
+function watch() {
+    gulp.watch('./scss/**/*.scss', compilaSass)
+}
+
+gulp.task('default', watch);
