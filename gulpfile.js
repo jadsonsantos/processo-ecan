464d7b0d6f34436465b0e043c1fa3da0ebaafce6
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-04-09 16:47:18 -0300

    Add gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(gulp.dest('./css'));
+});
