a5ae9a1a422197f726627e201cbead584cfa20f5
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-04-09 19:17:59 -0300

    Add browserSync para gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,29 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
 
 
 function compilaSass() {
     return gulp.src('./scss/**/*.scss')
       .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-      .pipe(gulp.dest('./css'));
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());;
 }
+
+function browser() {
+    browserSync.init({
+      server: {
+        baseDir: "./"
+      }
+    });
+}
+
+gulp.task('browser-sync', browser);
 
 function watch() {
     gulp.watch('./scss/**/*.scss', compilaSass)
 }
 
-gulp.task('default', watch);
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
