8db35541d1bba9546a22102065f115fab3e8abb7
Jadson dos Santos <jadsondossantos5@gmail.com>
2019-04-11 12:50:48 -0300

    Add estilo para produtos

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/** Oculta a estrutura para preço antigo no HTML, caso ele não exista */
+function verificaSeExistePrecoAntigo() {
+
+    var precoSemDesconto = document.getElementsByClassName('preco-sem-desconto');
+
+    for (var i = 0; i < precoSemDesconto.length; i++) {
+
+        var texto = precoSemDesconto[i].innerText;
+
+        if (texto.length == 0) {
+            precoSemDesconto[i].classList.add('inactive');
+        }
+    }
+}
+
+verificaSeExistePrecoAntigo();
